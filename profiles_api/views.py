0d9d6efdd5f968750e003f5580a7d2bb6aa5e735
Anuj Lahoty <anujlahoty1997@gmail.com>
2022-04-10 14:03:51 +0530

    Final version prepared and ready for deployment

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -9,7 +9,22 @@ from profiles_api import permissions
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated
+    )
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
 
 class UserLoginApiView(ObtainAuthToken):
    """Handle creating user authentication tokens"""
@@ -116,4 +131,5 @@ class HelloApiView(APIView):
         """Delete an object"""
 
         return Response({'method': 'DELETE'})
+
 
